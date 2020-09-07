f8e7c55c5a588fb089b0687430a9b0a368186791
Brian <blee11@uw.edu>
2020-09-07 12:49:50 -0700

    Set up test database

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -1,21 +1,39 @@
-import { v1 as uuid } from 'uuid';
+import database from '../firebase/firebase';
 
-export const addExpense = (
-  {
-    description = '',
-    note = '',
-    amount = 0,
-    createdAt = 0 } = {}
-  ) => ({
+// Not asynch redux action
+// component calls action generator
+// action generator returns object
+// component dispatches object
+// redux store updates
+
+// Asynch redux action
+// component calls action generator
+// action generator returns function
+// component dispatches function
+// function runs (can dispatch other actions)
+
+export const addExpense = (expense) => ({
   type: 'ADD_EXPENSE',
-  expense: {
-    id: uuid(),
-    description,
-    note,
-    amount,
-    createdAt
-  }
+  expense
 });
+
+export const startAddExpense = (expenseData = {}) => {
+  return (dispatch) => {
+    const {
+      description = "",
+      note = "",
+      amount = 0,
+      createdAt = 0
+    } = expenseData;
+    const expense = { description, note, amount, createdAt };
+    return database.ref('expenses').push(expense).then((ref) => {
+      dispatch(addExpense({
+        id: ref.key,
+        ...expense
+      }));
+    });
+  };
+}
 
 export const removeExpense = ({ id } = {}) => ({
   type: 'REMOVE_EXPENSE',
